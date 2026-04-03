2a11a365bfd1b4bbc072ec913a108495f6d65801
Ismail Sinan <ismailsinan2003@gmail.com>
2026-04-03 14:08:19 +0400

    authentication

---
+++ app/routes/auth.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
+export const meta = () => [
+  {
+    title: "Resumind | Auth",
+    name: "Description",
+    content: "log in to your Account",
+  },
+];
+
+const auth = () => {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate(next);
+  }, [auth.isAuthenticated, next]);
+
+  return (
+    <main className="bg-[url('app/public/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+          <div className="flex flex-col items-center gap-2 text-center">
+            <h1>Welcome</h1>
+            <h2>Log In to continue Your Job Journey </h2>
+          </div>
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you in...</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    {" "}
+                    <p>log out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>log in</p>
+                  </button>
+                )}
+              </>
+            )}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default auth;
